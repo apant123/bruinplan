38b38d2167487bf41fc458244ebab6c34c8eb00a
Arav Pant <aravpant17@gmail.com>
2026-04-17 14:14:53 -0700

    updated

--- frontend/src/components/ProfileSetup/ProfileSetup.js
+++ frontend/src/components/ProfileSetup/ProfileSetup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useAuth } from '../../contexts/AuthContext';
+import { API_BASE } from '../../api/constants';
 import NavBar from '../NavBar';
 import ProgressBar from './ProgressBar';
 import WelcomePage from './WelcomePage';
